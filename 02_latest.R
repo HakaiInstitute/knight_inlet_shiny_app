ad92912eb672ff9a87e687322a527f53835507f9
aschmill <144475756+aschmill@users.noreply.github.com>
2024-03-05 09:54:23 -0800

    Add files via upload

--- 02_latest.R
+++ 02_latest.R
@@ -16,6 +16,19 @@ pal_rich <- colorNumeric(
   domain = site_coords$sppr)
 
 
+
+# Footer  -----
+fluidRow(id = "footer",
+         column(2),
+         column(8,
+                HTML("<center><p style='size:18px';>
+                     App Development by <a href='https://jakelawlor.github.io/' target='_blank'>Jake Lawlor</a><br>
+                     </p></center>")),
+         column(2)
+         
+) # end footer fluidrow
+
+
 ui <- dashboardPage(
   dashboardHeader(title = "Biodiveristy in the Gwaxdlala/Nalaxdlala IPCA Marine Refuge", 
                   titleWidth = 600),  # since we have a long title, we need to extend width element in pixels
@@ -25,24 +38,18 @@ ui <- dashboardPage(
     fluidRow(
       box(title = "Map", status = "primary", solidHeader = TRUE,
           leafletOutput("map", width = "100%", height = "600px"), width = 8),
-      box(title = "Species Selector", 
-          status = "warning",           
-          collapsible = T,
-          solidHeader = T,
-          width = 4,
-          ### 1. Species Select Box -----
+      box(title = "Species Selector", status = "warning", solidHeader = TRUE,
           pickerInput(
-            inputId = "selectcalvertspp",
-            label = "Select a species", 
+            inputId = "speciesSelector",
+            label = "Select a species:", 
             # choices = full_spp_list,
-            choices = split(site_spp_list,  # need to make a full_spp_list for this to work
-                            names(site_spp_list)),  # was "full_spp_list"
+            choices = species_list, 
             multiple = TRUE,
             options = pickerOptions(liveSearch=T,
                                     maxOptions = 1),
-            choicesOpt = list(content = site_spp_list),  # was full_spp_list
+            choicesOpt = list(content = species_list),
             
-          )),  # Placeholder for species choices
+          ), choices = NULL, width = 4),  # Placeholder for species choices
       box(title = "Color Code by Species Richness", status = "warning", solidHeader = TRUE,
           radioGroupButtons(
             inputId = "colorCode",
@@ -56,7 +63,7 @@ ui <- dashboardPage(
           ),
           width = 4),
       box(title = "Species List", status = "warning", solidHeader = TRUE,
-          DTOutput("speciesTable", width = "100%"), width = 4)
+          reactableOutput("speciesTable", width = "100%"), width = 4)
     ),
     
     fluidRow(id = "footer",
@@ -71,30 +78,22 @@ ui <- dashboardPage(
   )
 )
 
+
+
+
 server <- function(input, output, session) {
   
-  # Populate the species selector dropdown with species names
-  # Assuming site_spp_list is a named list with species per site
-  all_species <- unique(unlist(site_spp_list))
   
-  all_species_formatted <- sapply(all_species, function(name) {
-    sprintf("<i>%s</i>", name)
-  })
   
-  observe({
-    updateSelectInput(session, "speciesSelector", choices = all_species_formatted)
-    session$sendCustomMessage('formatDropdown', list(id = 'speciesSelector'))
-  })
-  
-  # Render map 
+  # Render map -----------------------------------------------------------------
   
   output$map <- renderLeaflet({
     
     leaflet(
       options = leaflet::leafletOptions()) %>%
       addProviderTiles(providers$CartoDB.Voyager) %>%
+      addMapPane("yellow outlines", zIndex=600) %>% 
       addMapPane("depth points",zIndex=500) %>%
-      
       
       addCircleMarkers(data = site_coords,
                        lat = ~lat,
@@ -140,7 +139,7 @@ server <- function(input, output, session) {
   
   myLeafletProxy <- leafletProxy(mapId = "map", session)
   
-  # add richness layer when group button is clicked
+  # add sampledepth layer when group button is clicked
   observe({
     req("richness" %in% input$colorCode)
     leafletProxy("map") %>%
@@ -163,65 +162,87 @@ server <- function(input, output, session) {
       clearControls() %>%
       #removeControl(layerId = c("total_spp","depthlegend")) %>%
       
-      hideGroup(c("total_spp"))
+      hideGroup(c("richness"))
   })
   
-
   
-  # track clicked dot -------------------------------------------------------
-  # Highlight sites based on selected species
+  # Species selector -----------------------------------------------------------
+  # this section creates yellow borders around sites where a
+  # selected species is found. 
+  sites_containing_spp <- reactive({
+    req(input$speciesSelector)
+    names(site_spp_list)[sapply(seq_along(site_spp_list),function(x){input$speciesSelector %in% site_spp_list[[x]]})]
+  })
+  
   observeEvent(input$speciesSelector, {
-    selected_species <- input$speciesSelector
     
-    sites_containing_selected_species <- names(site_spp_list)[sapply(site_spp_list, function(species_list) {
-      selected_species %in% species_list
-    })]
+    leafletProxy("map") %>% 
+      clearGroup( "selected_sites")
     
-    leafletProxy("map") %>%
-      clearGroup("selected_sites") %>%
-      addCircleMarkers(data = site_coords[site_coords$site %in% sites_containing_selected_species, ],
-                       ~lat, ~long,
-                       weight = 3,
-                       radius = 5,
-                       fillColor = 'transparent',
-                       color = "yellow",
-                       opacity = 1,
-                       fillOpacity = .8,
-                       options = markerOptions(interactive = FALSE),
-                       group = "selected_sites")
-  }, ignoreNULL = FALSE)
-  
-  
-  # Initialize species table as empty, allowing HTML content
-  output$speciesTable <- renderDT({
-    datatable(data.frame(Species = character(0)), options = list(pageLength = 5, scrollX = TRUE), escape = FALSE)
-  }, server = FALSE)
-  
-  # Update table based on map click
-  observeEvent(input$map_marker_click, {
-    clicked_site <- input$map_marker_click$id
-    
-    if (!is.null(clicked_site) && clicked_site %in% names(site_spp_list)) {
-      species_list <- site_spp_list[[clicked_site]]
+    if(!is.null(input$speciesSelector))
+    {# %>%
       
-      # Assuming species_list contains strings formatted as "<i>Scientific name</i> - Common Name"
-      species_data <- data.frame(Species = species_list, stringsAsFactors = FALSE)
+      #clearMarkerClusters()
+      #   this works but try to make reactive
+      #    sites_containing_spp <- 
+      #      names(site_spp)[sapply(seq_along(site_spp),function(x){input$selectcalvertspp %in% site_spp[[x]]$species})]
       
-      # Render the species table with HTML formatting for scientific names
-      output$speciesTable <- renderDT({
-        datatable(species_data, 
-                  options = list(
-                    pageLength = -1, 
-                    scrollX = TRUE,
-                    scrollY = "200px",  # Set the height of the table's vertical viewport
-                    lengthChange = FALSE, 
-                    searching = FALSE,
-                    dom = 't'  # This option removes pagination controls
-                    ), 
-                  escape = FALSE)
-      }, server = FALSE)
+      leafletProxy("map") %>% 
+        addCircleMarkers(data = site_coords[site_coords$site %in% sites_containing_spp(),],
+                         lat = ~lat,
+                         lng = ~long,
+                         weight = 3,
+                         radius = 5,
+                         #layerId = ~site,
+                         fillColor = 'transparent',
+                         color = "yellow",
+                         opacity = 1,
+                         #popup = paste("<b>Site:</b>", site_coords$site," <br>
+                         #           <b>Species:</b>", site_coords$total_spp),
+                         fillOpacity = .8,
+                         options = markerOptions(interactive = FALSE,
+                                                 pane = "yellow outlines"),
+        ) 
+      
     }
+  }, ignoreNULL = F) 
+  
+  
+  
+  
+  # Initialize species table as empty, allowing HTML content -------------------
+  # Reactive value to store selected site
+  selectedSite <- reactiveVal()
+  
+  # Observe map click events
+  observeEvent(input$map_marker_click, {
+    selectedSite(input$map_marker_click$id)
   })
+  
+  # Render species list in reactable based on selected site
+  output$speciesTable <- renderReactable({
+    site <- selectedSite()  # Get the currently selected site
+    if (is.null(site)) {
+      species <- data.frame(Species = character(0))  # Empty data frame if no site is selected
+    } else {
+      species <- data.frame(Species = full_spp_list[[site]])
+    }
+    reactable(species,
+              pagination = F,
+              compact = T,
+              borderless = TRUE,
+              columns = list(Species = colDef(html = TRUE)))
+  })
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }
 
 
